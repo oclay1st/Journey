9764070ee7b56a5634a37d368ff1656f77b3cdd1
oclay <ocleidyreve@gmail.com>
2020-07-18 15:01:11 -0400

    feat: dummy security setup, free for all

--- src/main/java/com/smart/life/config/SecurityConfig.java
+++ src/main/java/com/smart/life/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.smart.life.admin.domain.user.AppUserDetailService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -29,4 +30,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         auth.userDetailsService(appUserDetailService).passwordEncoder(passwordEncoder());
     }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().permitAll();
+    }
 }
