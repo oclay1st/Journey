c22691fcef793fefc07dbbda8136ee306475e437
oclay <ocleidyreve@gmail.com>
2020-07-18 22:08:00 -0400

    feat: add api doc to fleet model controller

--- src/main/java/com/smart/life/saas/web/fleet/FleetModelController.java
+++ src/main/java/com/smart/life/saas/web/fleet/FleetModelController.java
@@ -3,19 +3,26 @@ package com.smart.life.saas.web.fleet;
 import com.smart.life.saas.domain.core.fleet.FleetModel;
 import com.smart.life.saas.domain.core.fleet.FleetService;
 import com.smart.life.saas.web.fleet.dto.FleetModelDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
 @RestController
+@Tag(name = "Fleet Models")
 @RequestMapping("/fleetModels")
 public class FleetModelController {
 
@@ -25,14 +32,16 @@ public class FleetModelController {
         this.fleetService = fleetService;
     }
 
-    @PostMapping
-    public ResponseEntity<FleetModel> save(@Valid FleetModelDTO fleetModelDTO) {
+    @PostMapping(consumes = { MediaType.MULTIPART_FORM_DATA_VALUE })
+    @Operation(summary = "Create a new fleet model")
+    @ApiResponses(value = { @ApiResponse(description = "A fleet model resource") })
+    public ResponseEntity<FleetModel> save(@Valid @ModelAttribute FleetModelDTO fleetModelDTO) {
         return new ResponseEntity<>(new FleetModel(), HttpStatus.CREATED);
     }
 
     @GetMapping
-    public ResponseEntity<Page<FleetModel>> listModels() {
-        Pageable pageable = PageRequest.of(1, 100);
-        return ResponseEntity.ok(fleetService.findAllModels(pageable));
+    @Operation(summary = "List fleet models")
+    public ResponseEntity<Page<FleetModel>> listModels(@RequestParam PageRequest pageRequest) {
+        return ResponseEntity.ok(fleetService.findAllModels(pageRequest));
     }
 }
--- src/main/java/com/smart/life/saas/web/fleet/dto/FleetModelDTO.java
+++ src/main/java/com/smart/life/saas/web/fleet/dto/FleetModelDTO.java
@@ -2,12 +2,23 @@ package com.smart.life.saas.web.fleet.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 
 @Setter
 @Getter
 public class FleetModelDTO implements Serializable {
     private static final Long serialVersionUID = 1L;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    private boolean active;
+
+    @NotNull
+    private MultipartFile thumbImageFile;
 
 }
