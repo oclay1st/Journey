291594720fa7713e61429d86c069e0a36c7bf693
oclay <ocleidyreve@gmail.com>
2021-04-26 14:56:54 -0400

    feat: add date and datetime default format

--- src/main/java/com/smart/life/config/JourneyConfig.java
+++ src/main/java/com/smart/life/config/JourneyConfig.java
@@ -1,4 +1,23 @@
 package com.smart.life.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.datetime.standard.DateTimeFormatterRegistrar;
+import org.springframework.format.support.DefaultFormattingConversionService;
+import org.springframework.format.support.FormattingConversionService;
+
+import java.time.format.DateTimeFormatter;
+
+@Configuration
 public class JourneyConfig {
+
+    @Bean
+    public FormattingConversionService conversionService(){
+        DefaultFormattingConversionService conversionService = new DefaultFormattingConversionService();
+        DateTimeFormatterRegistrar registrar = new DateTimeFormatterRegistrar();
+        registrar.setDateFormatter(DateTimeFormatter.ISO_DATE);
+        registrar.setDateTimeFormatter(DateTimeFormatter.ISO_DATE_TIME);
+        registrar.registerFormatters(conversionService);
+        return conversionService;
+    }
 }
