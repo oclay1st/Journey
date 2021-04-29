c8637a2b9e4f7f430415684171d328b25b780753
oclay <ocleidyreve@gmail.com>
2021-04-29 17:24:34 -0400

    refactor: remove flyway config

--- src/main/java/com/smart/life/JourneyApplication.java
+++ src/main/java/com/smart/life/JourneyApplication.java
@@ -1,21 +1,15 @@
 package com.smart.life;
 
-import org.flywaydb.core.Flyway;
-import org.flywaydb.core.api.configuration.FluentConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class JourneyApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JourneyApplication.class, args);
     }
 
-    @Bean(name = "flyway")
-    public Flyway flyawayConfig() {
-        final FluentConfiguration configuration = new FluentConfiguration();
-        return new Flyway(configuration);
-    }
 }
