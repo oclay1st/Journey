950a3f85e37885f7e80f252404ffa4531f0bf50b
oclay <ocleidyreve@gmail.com>
2021-05-01 23:15:40 -0400

    refactor: change dataSource properties loading

--- src/main/java/com/smart/life/config/AdminDataSourceConfig.java
+++ src/main/java/com/smart/life/config/AdminDataSourceConfig.java
@@ -1,11 +1,14 @@
 package com.smart.life.config;
 
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
@@ -18,33 +21,31 @@ import java.util.Objects;
 
 @Configuration
 @EnableJpaRepositories(entityManagerFactoryRef = "adminEntityManagerBean", transactionManagerRef = "adminTransactionManagerBean", basePackages = "com.smart.life.admin.infrastructure")
+@EnableConfigurationProperties(DataSourceProperties.class)
 public class AdminDataSourceConfig {
 
-    private Environment env;
+    private DataSourceProperties dataSourceProperties;
 
-    public AdminDataSourceConfig(Environment env) {
-        this.env = env;
+    public AdminDataSourceConfig(DataSourceProperties dataSourceProperties) {
+        this.dataSourceProperties = dataSourceProperties;
     }
 
     @Primary
     @Bean(name = "adminDatasourceBean")
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setUrl(env.getProperty("spring.datasource.url"));
-        dataSource.setUsername(env.getProperty("spring.datasource.username"));
-        dataSource.setPassword(env.getProperty("spring.datasource.password"));
-        dataSource.setDriverClassName(Objects.requireNonNull(env.getProperty("spring.datasource.driver-class-name")));
+        dataSource.setUrl(dataSourceProperties.getUrl());
+        dataSource.setUsername(dataSourceProperties.getUsername());
+        dataSource.setPassword(dataSourceProperties.getPassword());
+        dataSource.setDriverClassName(Objects.requireNonNull(dataSourceProperties.getDriverClassName()));
         return dataSource;
     }
 
     @Primary
     @Bean(name = "adminEntityManagerBean")
     public LocalContainerEntityManagerFactoryBean entityManagerFactory(EntityManagerFactoryBuilder builder,
-                                                                       @Qualifier("adminDatasourceBean") DataSource dataSource) {
-        return builder.dataSource(dataSource)
-                .packages("com.smart.life.")
-                .persistenceUnit("adminEntityManager")
-                .build();
+            @Qualifier("adminDatasourceBean") DataSource dataSource) {
+        return builder.dataSource(dataSource).packages("com.smart.life.").persistenceUnit("adminEntityManager").build();
     }
 
     @Primary
@@ -52,5 +53,12 @@ public class AdminDataSourceConfig {
     public PlatformTransactionManager transactionManager(
             @Qualifier("adminEntityManagerBean") EntityManagerFactory entityManagerFactory) {
         return new JpaTransactionManager(entityManagerFactory);
+    }
+
+    @Primary
+    @Bean(name = "liquibaseProperties")
+    @ConfigurationProperties(prefix = "spring.liquibase")
+    public LiquibaseProperties liquibaseProperties() {
+        return new LiquibaseProperties();
     }
 }
