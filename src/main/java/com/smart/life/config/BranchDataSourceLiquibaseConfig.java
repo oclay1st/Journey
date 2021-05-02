a52c1e3146597be09edd7381a23fab566bd085ea
oclay <ocleidyreve@gmail.com>
2021-05-01 23:17:00 -0400

    refactor: create new bean for liquibase branch dataSource

--- src/main/java/com/smart/life/config/BranchDataSourceLiquibaseConfig.java
+++ src/main/java/com/smart/life/config/BranchDataSourceLiquibaseConfig.java
@@ -2,28 +2,28 @@ package com.smart.life.config;
 
 import java.util.Map;
 
-
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
 
 @Configuration
 @ConditionalOnProperty(prefix = "journey.liquibase.branch", name = "enable", matchIfMissing = true)
-@EnableConfigurationProperties(LiquibaseProperties.class)
 public class BranchDataSourceLiquibaseConfig {
 
-    private LiquibaseProperties properties;
-    
-    BranchDataSourceLiquibaseConfig(LiquibaseProperties properties){
-        this.properties = properties;
+    @Bean(name = "branchLiquibaseProperties")
+    @ConfigurationProperties(prefix = "journey.liquibase.branch")
+    public LiquibaseProperties branchLiquibaseProperties() {
+        return new LiquibaseProperties();
     }
 
     @Bean
     @DependsOn("branchDataSourceBean")
-    public BranchDataSourceLiquibase branchDataSourceLiquibase(Map<Object, Object> targetDataSources) {
-        return new BranchDataSourceLiquibase(targetDataSources, properties);
+    public BranchDataSourceLiquibase branchDataSourceLiquibase(Map<Object, Object> targetDataSources,
+            @Qualifier("branchLiquibaseProperties") LiquibaseProperties branchLiquibaseProperties) {
+        return new BranchDataSourceLiquibase(targetDataSources, branchLiquibaseProperties);
     }
 }
