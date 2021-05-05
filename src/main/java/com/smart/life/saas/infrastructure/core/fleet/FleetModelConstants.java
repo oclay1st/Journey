ca45de082f8fe1d17740643ceaefab2d068b1dcc
oclay <ocleidyreve@gmail.com>
2021-05-05 09:59:15 -0400

    refactor: change fleet model base urls

--- src/main/java/com/smart/life/saas/infrastructure/core/fleet/FleetModelConstants.java
+++ src/main/java/com/smart/life/saas/infrastructure/core/fleet/FleetModelConstants.java
@@ -3,7 +3,7 @@ package com.smart.life.saas.infrastructure.core.fleet;
 public class FleetModelConstants {
 
     public static final String MODEL_THUMB_IMAGE_PATH = "modelImages";
-    public static final String BASE_PATH = "/fleetModels";
+    public static final String BASE_PATH = "/fleets/models";
     public static final String GET_BY_ID_PATH = "/{id}";
 
     private FleetModelConstants() {
