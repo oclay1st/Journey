0083a4a6a2bb04e559f1705556ef429787a19162
oclay <ocleidyreve@gmail.com>
2021-05-10 18:02:14 -0400

    refactor: move logic that set the default branch data source logic into data source routing

--- src/main/java/com/smart/life/config/BranchRoutingAwareDataSource.java
+++ src/main/java/com/smart/life/config/BranchRoutingAwareDataSource.java
@@ -1,5 +1,7 @@
 package com.smart.life.config;
 
+import java.util.Map;
+
 import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
 
 public class BranchRoutingAwareDataSource extends AbstractRoutingDataSource {
@@ -7,5 +9,14 @@ public class BranchRoutingAwareDataSource extends AbstractRoutingDataSource {
     @Override
     protected Object determineCurrentLookupKey() {
         return BranchContextHolder.get();
+    }
+
+    @Override
+    public void setTargetDataSources(Map<Object, Object> targetDataSources) {
+        if (BranchContextHolder.get() == null) {
+            String firstDataSource = String.valueOf(targetDataSources.keySet().iterator().next());
+            BranchContextHolder.set(firstDataSource);
+        }
+        super.setTargetDataSources(targetDataSources);
     }
 }
